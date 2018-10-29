f2207ab8010f0444a042e22aa55f5f0b95e8c5de
MettaliK <42770217+MettaliK@users.noreply.github.com>
2018-10-29 18:45:23 +0100

    Remove passgenlib
    As it is no longer used, functions has been implemented to passgen.py

--- passgenlib.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_folder_size(start_path): #Gets size of folder in Bytes (recursive)
-    total_size = 0
-    for dirpath, filenames in os.walk(start_path):
-        for f in filenames:
-            fp = os.path.join(dirpath, f)
-            total_size += os.path.getsize(fp)
-    return total_size
