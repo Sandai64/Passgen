4006255758d6d92492c02b61b334e0ea0e86ade3
MettaliK <42770217+MettaliK@users.noreply.github.com>
2019-02-16 19:08:30 +0100

    remove passgenlib dependency in BUILD.bat

--- BUILD.bat
+++ BUILD.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-python cxfreeze -OO -c --include-modules=string,os,sys,random,time,io,secrets,base64,decimal,colorama,termcolor,passgenlib passgen.py
+python cxfreeze -OO -c --include-modules=string,os,sys,random,time,io,secrets,base64,decimal,colorama,termcolor passgen.py
 
 pause
